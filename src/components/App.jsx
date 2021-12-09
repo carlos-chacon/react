8d34ede5375dec2999cb2d5d12460a40190faf04
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2021-12-09 10:55:59 -0500

    maquetacion react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
     return (
         <div>
-            Hola Mundo
+            <Layout children={undefined}>
+                <Login />
+            </Layout>
         </div>
     )
 }
