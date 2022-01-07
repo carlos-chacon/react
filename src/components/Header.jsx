52ef36a75b4a9383a7fb9346341b9cb86be3b1af
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-01-07 10:49:06 -0500

    Orden de compra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import Menu from "@components/Menu";
+import MyOrder from "../containers/MyOrder";
 import AppContext from "@context/AppContext";
 import "@styles/Header.scss";
 
@@ -9,6 +10,7 @@ import logoShopping from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
@@ -45,13 +47,14 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
             <img src={logoShopping} alt="shopping cart" />
             {state.cart.length > 0 && <div>{state.cart.length}</div>}
           </li>
         </ul>
       </div>
       {toggle ? <Menu /> : null}
+      {toggleOrders ? <MyOrder /> : null}
     </nav>
   );
 };
